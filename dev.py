c1eec479c86d7194a79927fcb0c012f90c0b033a
ApartTUSITU <aptusitu@outlook.com>
2024-11-23 00:36:00 +0800

    ✨ feat: (not finished)角色生日提醒

--- dev.py
+++ dev.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import os
+import json
+from datetime import datetime
 
 import botpy
 from botpy import logging
 from botpy.ext.cog_yaml import read
 from botpy.message import GroupMessage, Message, C2CMessage
 
-test_config = read(os.path.join(os.path.dirname(__file__), "appinfo.yaml"))
+version = 0.1
+
+appinfo = read(os.path.join(os.path.dirname(__file__), "appinfo.yaml"))
 
 _log = logging.get_logger()
 
+# 读取 JSON 文件
+with open('characters.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# 获取今天的日期
+today = datetime.now().strftime('%m-%d')
 
 class MyClient(botpy.Client):
     async def on_ready(self):
@@ -28,14 +38,14 @@ class MyClient(botpy.Client):
         await message._api.post_c2c_message(
             openid=message.author.user_openid, 
             msg_type=0, msg_id=message.id, 
-            content=f"我收到了你的消息：{message.content}")
+            content=f"收到了消息：{message.content}")
     
     async def on_at_message_create(self, message: Message):
         _log.info(message.author.avatar)
         if "sleep" in message.content:
             await asyncio.sleep(10)
         _log.info(message.author.username)
-        await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}")
+        await message.reply(content=f"收到了消息： {message.content}")
 
 
 if __name__ == "__main__":
@@ -46,4 +56,4 @@ if __name__ == "__main__":
     # 通过kwargs，设置需要监听的事件通道
     intents = botpy.Intents(public_messages=True,public_guild_messages=True)
     client = MyClient(intents=intents)
-    client.run(appid=test_config["appid"], secret=test_config["secret"])
+    client.run(appid=appinfo["appid"], secret=appinfo["secret"])
